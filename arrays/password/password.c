be0d4592065ab0991f20c58e238e26ada7103959
3dgyGamer <jakepark2908@gmail.com>
2024-06-12 00:42:48 -0400

    added the stuff I did

---
+++ arrays/password/password.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+    char password[50];
+    int counter = 0;
+    printf("Please enter a password: ");
+    scanf("%s", password);
+    //password checks
+    //check if one uppercase letter
+    for (int i=0; i < strlen(password); i++){
+        
+    }
+}
+
